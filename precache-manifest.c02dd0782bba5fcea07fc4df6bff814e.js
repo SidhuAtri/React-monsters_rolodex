ff3e0cb343a6b98a46b17be65ba800b490b51609
sidharth atri <sidharthatri@gmail.com>
2020-04-26 14:13:08 +0530

    Updates

---
+++ precache-manifest.c02dd0782bba5fcea07fc4df6bff814e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd328a04d6072e5e8a20d771e892bdfd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1b5baf9c18b864d1d41",
+    "url": "/monsters-rolodex/static/css/main.eea44247.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5baf9c18b864d1d41",
+    "url": "/monsters-rolodex/static/js/main.6d2c4405.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
